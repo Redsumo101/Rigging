ee5b068fa4b6f5732509840ea55b7fce3f726bbc
Riley <rileyharper15@gmail.com>
2022-01-12 16:00:20 -0700

    New scripts/joint placements and parenting

--- MayaScripts/PlaceCenterOfLoop.py
+++ MayaScripts/PlaceCenterOfLoop.py
@@ -11,4 +11,21 @@ def locator():
         
     cmds.xform(loc, translation=[mid_x, mid_y, mid_z], ws = True)
     
-locator()
+
+
+def joints_from():
+    sels = cmds.ls(sl =True)
+    
+    for sel in sels:
+        pos = cmds.xform(sel, q = True, rotatePivot=True, ws = True)
+        cmds.select(cl = True)
+        n_joint = cmds.joint(position=[0,0,0], absolute = True)
+        cmds.xform(n_joint, translation=pos, ws= True)
+        
+def parent_from_sels():
+    sels = cmds.ls(sl = True)
+    for i, sel in enumerate(sels):
+        if i <len(sels) - 1:
+            cmds.parent(sels[i], sels[i+1])
+
+        
